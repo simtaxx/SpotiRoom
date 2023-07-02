c712782a678317d8535128d50fa0fa6641bbe351
Haris Souici <souici.haris@gmail.com>
2023-07-02 15:52:23 +0200

    [FEATURE] Open user auth in new page

--- client/src/api/services/serviceAuthentication.tsx
+++ client/src/api/services/serviceAuthentication.tsx
@@ -5,9 +5,37 @@ import {
     getUserInformations
 } from '../routes/routeAuthentication'
 
+interface IUser {
+    id: string
+    name: string
+    picture: string
+    product: string
+    type: string
+}
+
+interface IStoredUserInformations {
+    access_token: string
+    refresh_token: string
+    token_type: string
+    user: IUser
+}
+
+const storeUserInformations = ({ access_token, refresh_token, token_type, user }: IStoredUserInformations) => {
+    localStorage.setItem('authAccessToken', `${token_type} ${access_token}`)
+    localStorage.setItem('authRefreshToken', refresh_token)
+    localStorage.setItem('user', JSON.stringify(user))
+}
+
+const manageAuthWindows = () => {
+    window.close()
+    window.onunload = () => {
+        window.opener.location.reload()
+    }
+}
+
 export const authenticate = async () => {
     const { redirectionURL } = await getAuthorization()
-    window.location = redirectionURL
+    window.open(redirectionURL as unknown as string,'name','width=600,height=800')
 }
 
 export const logout = () => {
@@ -17,12 +45,10 @@ export const logout = () => {
 
 export const fetchUser = async (code: string) => {
     const { access_token, refresh_token, token_type } = await getAccessToken(code)
-    const user = await getUserInformations(access_token)
+    const user: IUser = await getUserInformations(access_token)
 
-    localStorage.setItem('authAccessToken', `${token_type} ${access_token}`)
-    localStorage.setItem('authRefreshToken', refresh_token)
-    localStorage.setItem('user', JSON.stringify(user))
-    window.location.href = '/'
+    storeUserInformations({ access_token, refresh_token, token_type, user })
+    manageAuthWindows()
 }
 
 export const fetchRefreshAccessToken = async () => {
