613c86b15a08c40a4fe8406ef3b4ef56504593fa
Haris Souici <44004478+simtaxx@users.noreply.github.com>
2023-07-02 17:43:32 +0200

    [FEATURE] Add global loader (#5)

--- client/src/components/Albums/index.tsx
+++ client/src/components/Albums/index.tsx
@@ -1,30 +1,34 @@
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import styles from './styles.module.scss'
 import { fetchAlbums } from '@/api/services/serviceAlbums'
-import Image from 'next/image'
 import { IAlbum } from './interfaces'
 import { Album } from './Album'
+import { useDispatch } from 'react-redux'
+import { setIsAlbumsloading } from '@/store/loadingSlice'
 
 export const Albums = () => {
   const [albums, setAlbums] = useState<IAlbum[]>([])
+  const dispatch = useDispatch()
+
+  const storeAlbums = useCallback(async () => {
+    dispatch(setIsAlbumsloading(true))
+    const albums = await fetchAlbums()
+    setAlbums(albums)
+    dispatch(setIsAlbumsloading(false))
+  }, [dispatch])
 
   useEffect(() => {
     storeAlbums()
-  }, []) 
+  }, [storeAlbums]) 
 
-  const storeAlbums = async () => {
-    const albums = await fetchAlbums()
-    setAlbums(albums)
-  }
-
-  return (
+  return !!albums.length && (
     <section className={styles.albums}>
       <p className={styles.albums__title}>My albums</p>
-      <div className={styles.albums__list}>
-        {!!albums.length && (albums.map(({ album }) => (
-          <Album album={album} key={album.id} />
-        )))}
-      </div>
+        <div className={styles.albums__list}>
+          {albums.map(({ album }) => (
+            <Album album={album} key={album.id} />
+          ))}
+        </div>
     </section>
   )
 }
--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
-import userSlice from './authSlice'
+import loadingSlice from './loadingSlice'
 
 export default configureStore({
     reducer: {
-        user: userSlice
+        loading: loadingSlice
     }
 })
