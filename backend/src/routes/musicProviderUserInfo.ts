cf79c58f3a19f1028e04f15eff0681159ee2e99e
Haris Souici <44004478+simtaxx@users.noreply.github.com>
2023-07-08 21:08:02 +0200

    [FEATURE] Create services instance only once (#10)

--- backend/src/routes/musicProviderUserInfo.ts
+++ backend/src/routes/musicProviderUserInfo.ts
@@ -2,11 +2,11 @@ import { Router } from 'express'
 import { SpotifyAuthentication } from '../adapters/externalServices/spotify/authentication'
 
 const musicProviderUserInfo = Router()
+const spotifyAuthentication = new SpotifyAuthentication()
 
 musicProviderUserInfo.post('/', async (req, res) => {
   try {
     const { accessToken } = req.body
-    const spotifyAuthentication = new SpotifyAuthentication()
     const user = await spotifyAuthentication.fetchUser(accessToken)
 
     res.send(user)
